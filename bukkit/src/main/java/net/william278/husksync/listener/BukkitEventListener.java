b01a944f0c472ec80765fb9ae37710155551fefe
Emibergo02 <36164338+Emibergo02@users.noreply.github.com>
2026-09-17 11:13:54 +0200

    Replace Jedis client library with Lettuce (#646)
    Implement a retry system when redis commands fail
    Add wait for async player saves on server shutdown
    Add logging for exceptions during pending player saves
    Add setting for server shutdown timeout duration
    
    (cherry picked from commit 8fec553a3f3421271929e9ffead0dc914672e1a8)

--- bukkit/src/main/java/net/william278/husksync/listener/BukkitEventListener.java
+++ bukkit/src/main/java/net/william278/husksync/listener/BukkitEventListener.java
@@ -122,9 +122,9 @@ public class BukkitEventListener extends EventListener implements BukkitJoinEven
         }
 
         // Handle saving player data snapshots when the world saves
-        plugin.runAsync(() -> super.saveOnWorldSave(event.getWorld().getPlayers()
+        super.saveOnWorldSave(event.getWorld().getPlayers()
                 .stream().map(player -> BukkitUser.adapt(player, plugin))
-                .collect(Collectors.toList())));
+                .collect(Collectors.toList()));
     }
 
     @EventHandler(ignoreCancelled = true)
